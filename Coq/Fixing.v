ba50277400932021ed8b7f1cc5aee7027fad37e9
Patrick Bahr <patrick.bahr@gmail.com>
2014-09-23 09:43:42 +0200

    simplify definition of contract fixing

--- Coq/Fixing.v
+++ Coq/Fixing.v
@@ -12,48 +12,50 @@ Definition constFoldBin {n} (op : BinOp) (e1 e2 : rexp' n) : rexp' n :=
   end.
 
 
-Fixpoint constFoldAcc {V} (f : nat -> Z -> rexp' V)  (l : nat) (z : rexp' V) : 
+Fixpoint constFoldAcc {V} (rho : obs) (f : obs -> Z -> rexp' V)  (l : nat) (z : obs -> rexp' V) :
   rexp' V + (nat * rexp' V) :=
   match l with
-    | O => inl z
-    | S l' => match constFoldAcc f l' z with
-                | inl (RLit _ r) => inl (f l r)
+    | O => inl (z rho)
+    | S l' => match constFoldAcc (adv_inp (-1) rho) f l'  z with
+                | inl (RLit _ r) => inl (f rho r)
                 | inl e => inr (O, e)
                 | inr (n, e) => inr (S n, e)
               end
 end.
 
+Definition obs_empty : obs := fun _ _ => None.
 
-                   
 Fixpoint Rfixing' {V V'} (e : rexp' V) (rho : obs) : PEnv Z V V' -> rexp' V' :=
     match e with
       | RLit _ r => fun _ => RLit r
-      | RBin n op e1 e2 => fun vars => constFoldBin op (Rfixing' e1 rho vars) (Rfixing' e2 rho vars)
-      | RNeg _ e => fun vars => 
+      | RBin _ op e1 e2 => fun vars => constFoldBin op (Rfixing' e1 rho vars) (Rfixing' e2 rho vars)
+      | RNeg _ e => fun vars =>
                       match Rfixing' e rho vars with
-                        | RLit _ r => RLit (0 - r)
-                        | e => e
+                        | RLit _ r => RLit (- r)
+                        | e => RNeg e
                       end
-      | Obs _ obs t => fun vars => 
+      | Obs _ obs t => fun vars =>
                          match rho t obs with
                            | Some r => RLit r
                            | None => Obs obs t
                          end
-      | RVar _ v => fun vars => 
+      | RVar _ v => fun vars =>
                       match lookupPEnv vars v with
                         | inl r =>  RLit r
                         | inr v' => RVar v'
                       end
-      | RAcc _ f l z => fun vars => 
-                        let rho' := adv_inp (0 - Z.of_nat l) rho in
-                        let f' i z := Rfixing' f (adv_inp (Z.of_nat i)  rho') (PExtend z vars)
-                        in match constFoldAcc f' l (Rfixing' z rho' vars) with
+      | RAcc _ f l z => fun vars =>
+                        let z' rho := Rfixing' z rho vars in
+                        let f' rho r := Rfixing' f rho (PExtend r vars)
+                        in match constFoldAcc rho f' l z' with
                              | inl e => e
-                             | inr (l', e') => RAcc (Rfixing' f (fun _ _ => None) (PSkip vars)) l' e'
+                             | inr (l', e') => RAcc (Rfixing' f obs_empty (PSkip vars)) l' e'
                            end
     end.
 
+
 Definition Rfixing (e : rexp) (o : obs) : rexp := Rfixing' e o (PEmpty (zero Z)).
+
 
 Fixpoint Bfixing (e : bexp) (rho : env) : bexp :=
   match e with
@@ -62,16 +64,16 @@ Fixpoint Bfixing (e : bexp) (rho : env) : bexp :=
                            | Some b => BLit b
                            | None => e
                          end
-    | RCmp cmp e1 e2 => match Rfixing e1 (fst rho), Rfixing e2 (fst rho) with 
-                          | RLit _ b1, RLit _ b2 => BLit (RCompare cmp b1 b2) 
+    | RCmp cmp e1 e2 => match Rfixing e1 (fst rho), Rfixing e2 (fst rho) with
+                          | RLit _ b1, RLit _ b2 => BLit (RCompare cmp b1 b2)
                           | _,_ => e
                         end
     | BNot e' => match Bfixing e' rho with
                    | BLit b => BLit (negb b)
                    | _ => e
                  end
-    | BOp op e1 e2 => match Bfixing e1 rho, Bfixing e2 rho with 
-                        | BLit b1, BLit b2 => BLit (BBinOp op b1 b2) 
+    | BOp op e1 e2 => match Bfixing e1 rho, Bfixing e2 rho with
+                        | BLit b1, BLit b2 => BLit (BBinOp op b1 b2)
                         | _,_ => e
                       end
   end.
