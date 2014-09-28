83d3b4f29c2bc891c4f2c96f17fbd32e17cfac85
Patrick Bahr <patrick.bahr@gmail.com>
2014-09-28 13:09:33 +0200

    started correctness proof for specialisation

--- Coq/Fixing.v
+++ Coq/Fixing.v
@@ -54,7 +54,74 @@ Fixpoint Rfixing' {V V'} (e : rexp' V) (rho : obs) : PEnv Z V V' -> rexp' V' :=
     end.
 
 
+Fixpoint constFoldAcc' {V V'} (vars: PEnv Z V V') (rho : obs) (f : Scope rexp' V) (l : nat) (z : rexp' V) :
+  rexp' V' + (nat * rexp' V') :=
+  match l with
+    | O => inl (Rfixing' z rho vars)
+    | S l' => match constFoldAcc' vars (adv_inp (-1) rho) f l' z with
+                | inl (RLit _ r) => inl (Rfixing' f rho (PExtend r vars))
+                | inl e => inr (O, e)
+                | inr (n, e) => inr (S n, e)
+              end
+end.
+
+
+Definition fixingRAcc {V V'} (f : Scope rexp' V) (l : nat) (z: rexp' V) (rho : obs) : PEnv Z V V' -> rexp' V' :=
+  fun vars =>
+    match constFoldAcc' vars rho f l z with
+      | inl e => e
+      | inr (l', e') => RAcc (Rfixing' f obs_empty (PSkip vars)) l' e'
+    end.
+
+Lemma fixingRAcc_def {V V'} (f : Scope rexp' V) (l : nat) (z: rexp' V) (rho : obs) (vars : PEnv Z V V') :
+  Rfixing' (RAcc f l z) rho vars = fixingRAcc f l z rho vars.
+Proof. admit.
+  (* induction l. *)
+  (* - reflexivity. *)
+  (* - unfold fixingRAcc. simpl. *)
+Qed.
+
+
 Definition Rfixing (e : rexp) (o : obs) : rexp := Rfixing' e o (PEmpty (zero Z)).
+
+Fixpoint subst {V V'} (e : rexp' V) : PEnv Z V V' -> rexp' V' :=
+    match e with
+      | RLit _ r => fun _ => RLit r
+      | RBin _ op e1 e2 => fun vars => RBin op (subst e1 vars) (subst e2 vars)
+      | RNeg _ e => fun vars => RNeg (subst e vars)
+      | Obs _ obs t => fun _ => Obs obs t
+      | RVar _ v => fun vars =>
+                      match lookupPEnv vars v with
+                        | inl r =>  RLit r
+                        | inr v' => RVar v'
+                      end
+      | RAcc _ f l z => fun vars => RAcc (subst f (PSkip vars)) l (subst z vars)
+    end.
+
+
+Lemma Rfixing_complete' {V V'} e rho vars (pvars : PEnv Z V V') :
+  R'[|Rfixing' e rho pvars|] vars obs_empty = R'[|subst e pvars|]vars rho.
+Proof.
+  generalize dependent rho. induction e; intros; auto.
+  - simpl.
+    remember (Rfixing' e1 rho pvars) as R1. pose (IHe1 pvars rho) as IH1.
+    remember (Rfixing' e2 rho pvars) as R2. pose (IHe2 pvars rho) as IH2.
+    rewrite <- HeqR1 in *. rewrite <- HeqR2 in *. rewrite <- IH1. rewrite <- IH2.
+    destruct R1; destruct R2; reflexivity.
+  - simpl. remember (Rfixing' e rho pvars) as R. pose (IHe pvars rho) as IH.
+    rewrite <- HeqR in *. rewrite <- IH.
+    destruct R; try reflexivity.
+  - simpl. destruct (rho z o); reflexivity.
+  - simpl. destruct (lookupPEnv pvars v); reflexivity.
+  - rewrite fixingRAcc_def. generalize dependent rho. generalize dependent pvars. induction n; intros.
+    + unfold fixingRAcc. simpl. rewrite IHe0. reflexivity.
+    + unfold fixingRAcc.
+      (* remember (constFoldAcc' pvars rho s (S n) e) as C. *)
+      (* destruct C. simpl in HeqC. *)
+      simpl.
+      remember (constFoldAcc' pvars (adv_inp (-1) rho) s n e) as R.
+      destruct R. destruct r. simpl.
+
 
 
 Fixpoint Bfixing (e : bexp) (rho : env) : bexp :=
