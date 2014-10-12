405a89b8f3a281756bece64094fdd4f42ff8bded
Patrick Bahr <patrick.bahr@gmail.com>
2014-10-12 21:14:24 +0300

    slightly stricter variant of syntactic horizon

--- Coq/Horizon.v
+++ Coq/Horizon.v
@@ -6,14 +6,14 @@ Require Import Tactics.
 Fixpoint horizon (c : contract) : nat :=
   match c with
       | Zero => 0
-      | TransfOne _ _ _ => 0
+      | TransfOne _ _ _ => 1
       | Scale _ c' => horizon c'
       | Transl l c' => l + horizon c'
       | Both c1 c2 => max (horizon c1) (horizon c2)
       | IfWithin _ l c1 c2 => l + max (horizon c1) (horizon c2)
   end.
 
-Lemma horizon_empty c rho i : horizon c  < i -> C[|c|]rho i = None \/ C[|c|]rho i = empty_trans.
+Lemma horizon_empty c rho i : horizon c  <= i -> C[|c|]rho i = None \/ C[|c|]rho i = empty_trans.
 Proof.
   generalize dependent rho. generalize dependent i. 
   induction c; simpl in *; intros.
@@ -24,11 +24,11 @@ Proof.
     + destruct (E[|e|] rho). 
       * right. rewrite H. apply scale_empty_trans. 
       * left. reflexivity.
-  - assert (horizon c < i - n) as H' by omega.
+  - assert (horizon c <= i - n) as H' by omega.
     eapply IHc in H'. 
     unfold delay_trace. assert (leb n i = true) as L. apply leb_correct. omega. rewrite L.
     destruct H'; eauto.
-  - rewrite NPeano.Nat.max_lub_lt_iff in H. destruct H as [H1 H2].
+  - rewrite NPeano.Nat.max_lub_iff in H. destruct H as [H1 H2].
     eapply IHc1 in H1. eapply IHc2 in H2.
     unfold add_trace, add_trans.
     destruct H1 as [H1|H1]. 
@@ -37,7 +37,7 @@ Proof.
       * left. rewrite H2. apply option_map2_none.
       * right. rewrite H1, H2. auto.  
   - rewrite <- Max.plus_max_distr_l in H.
-    rewrite NPeano.Nat.max_lub_lt_iff in H. destruct H as [H1 H2].
+    rewrite NPeano.Nat.max_lub_iff in H. destruct H as [H1 H2].
     generalize dependent rho. generalize dependent i. 
     induction n; intros.
     + eapply IHc1 in H1. eapply IHc2 in H2.
@@ -51,7 +51,7 @@ Proof.
         rewrite L. apply IHn; omega.      
 Qed.
 
-Theorem horizon_sound c rho i : horizon c  < i -> C[|c|]rho i ⊆ empty_trans.
+Theorem horizon_sound c rho i : horizon c  <= i -> C[|c|]rho i ⊆ empty_trans.
 Proof.
   simpl. intros R t T. apply horizon_empty with (rho:=rho) in R. destruct R as [R|R].
   tryfalse. rewrite <- R. assumption.
