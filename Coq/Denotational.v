361f944897bade02702baa1337ef953ce6777c9d
Patrick Bahr <patrick.bahr@gmail.com>
2014-11-04 17:06:16 +0100

    start on extrinsic formalisation
    This branch explores an extrinsic approach to formalising the contract
    language. That is, instead of representing contract language's type
    system using Coq's type system, we use explicit typing judgements. Our
    hypothesis is that this extrinsic approach makes code extraction
    simpler/possible.

--- Coq/Denotational.v
+++ Coq/Denotational.v
@@ -11,7 +11,17 @@ Infix "∘" := compose (at level 40, left associativity).
 
 (* Observations: mapping observables to values *)
 
-Definition ext := Z -> forall (A : Ty), ObsLabel A -> option [|A|].
+Definition ExtEnv := ObsLabel -> Z -> option Val.
+
+
+Reserved Notation "'|-V'' e '∶' t" (at level 20).
+
+Inductive TypeVal' : option Val -> Ty -> Prop := 
+| type_some v t : |-V v ∶ t -> |-V' Some v ∶ t
+| type_none t : |-V' None ∶ t
+        where "'|-V'' v '∶' t" := (TypeVal' v t).
+
+Definition TypeExt (rho : ExtEnv) := forall z l t, |-O l ∶ t -> |-V' (rho l z)  ∶ t.
 
 Definition eq_str (s1 s2 : string) : bool :=
   match string_dec s1 s2 with
@@ -39,71 +49,44 @@ Instance none_Partial A : Partial (option A) := {
   }.
 
 
-Instance option_Partial (f : Ty -> Type) t : Partial ((option ∘ f) t) := {
-  lep t1 t2  := forall z , t1 = Some z -> t2 = Some z
-  }.
 
-Lemma lep_some A (o : option A) x : Some x ⊆ o -> Some x = o.
-Proof.
-  simpl. intros. symmetry. auto.
-Qed. 
-
-Inductive EnvLe {f : Ty -> Type} : forall {V}, Env (option ∘ f) V -> Env (option ∘ f) V -> Prop :=
-| EnvLeNil : EnvLe (EnvNil _) (EnvNil _)
-| EnvLeExtend i V (e1 e2 : Env (option ∘ f) V) (x1 x2 : option (f i)) : 
-    x1 ⊆ x2 -> EnvLe e1 e2 (V:=V) -> EnvLe (EnvCons x1 e1) (EnvCons x2 e2).
-
-Hint Constructors EnvLe.
-
-Instance env_Partial (f : Ty -> Type) V : Partial (Env (option ∘ f) V) := {
-  lep := EnvLe
-  }.
-
-Lemma EnvLeEmpty' (f : Ty -> Type) : EnvNil (option ∘ f) ⊆ EnvNil (option ∘ f).
-constructor. Qed.
-
-Lemma EnvLeExtend' (f: Ty -> Type) V (e1 e2 : Env (option ∘ f) V) i (x1 x2 : option (f i)) :
-  x1 ⊆ x2 -> e1 ⊆ e2 -> EnvCons x1 e1 ⊆ EnvCons x2 e2.
-constructor; assumption. Qed.
-
-Lemma EnvLe_lookup {t} {V : list Ty} {f : Ty -> Type} (e1 e2 : Env (option ∘ f) V) (v : Var V t) : 
-  e1 ⊆ e2 -> lookupEnv v e1 ⊆ lookupEnv v e2.
-Proof.
-  intros L. induction L. simpl. auto. dependent destruction v. apply H. apply IHL. 
-Qed.
-
-Hint Resolve EnvLe_lookup EnvLeEmpty' EnvLeExtend'.
-
-Instance ext_Partial : Partial ext := {
-  lep t1 t2  := forall i t (o : ObsLabel t) (z : [|t|]) , t1 i t o = Some z -> t2 i t o = Some z
+Instance ext_Partial : Partial ExtEnv := {
+  lep t1 t2  := forall i (o : ObsLabel) (v : Val) , t1 o i = Some v -> t2 o i = Some v
   }.
 
 (* Move observations into the future. *)
 
-Definition adv_ext (d : Z) (e : ext) : ext
-  := fun x => e (d + x)%Z.
+Definition adv_ext (d : Z) (e : ExtEnv) : ExtEnv
+  := fun l x => e l (d + x)%Z.
 
-Lemma adv_ext_0 (e : ext) : adv_ext 0 e = e.
+Lemma adv_ext_0 (e : ExtEnv) : adv_ext 0 e = e.
 Proof.
   apply functional_extensionality.
   unfold adv_ext. reflexivity.
 Qed.
 
-Lemma adv_ext_iter d d' (e : ext) : adv_ext d (adv_ext d' e) = adv_ext (d' + d) e.
+Lemma adv_ext_iter d d' (e : ExtEnv) : adv_ext d (adv_ext d' e) = adv_ext (d' + d) e.
 Proof.
-  apply functional_extensionality. induction d'; intros.
+  apply functional_extensionality. intro. apply functional_extensionality. induction d'; intros.
   - simpl. rewrite adv_ext_0. reflexivity.
   - simpl. unfold adv_ext in *.  rewrite Z.add_assoc. reflexivity.
   - unfold adv_ext. rewrite Z.add_assoc.  reflexivity.
 Qed.
 
 
-Lemma adv_ext_swap d d' (e : ext) : 
+Lemma adv_ext_swap d d' (e : ExtEnv) : 
   adv_ext d (adv_ext d' e) = adv_ext d' (adv_ext d e).
 Proof.
   repeat rewrite adv_ext_iter. rewrite Z.add_comm. reflexivity.
 Qed.
 
+
+Lemma adv_ext_type e d : TypeExt e -> TypeExt (adv_ext d e).
+Proof.
+  unfold TypeExt, adv_ext. intros. auto.
+Qed.
+
+Hint Resolve adv_ext_type.
 
 (* Semantics of (real) binary operations. *)
 
@@ -124,13 +107,31 @@ Definition Reqb (x y : R) : bool := Rleb x y && Rleb y x.
 
 (* Lifts binary functions into [option] types. *)
 
-Definition option_map2 {A B C :Type} (f:A->B->C) o1 o2 :=
-  match o1,o2 with
-    | Some a, Some b => Some (f a b)
-    | _,_ => None
+
+Definition bind {A B} (x : option A) (f : A -> option B) : option B :=
+  match x with
+    | None => None
+    | Some x' => f x'
   end.
 
-Lemma option_map2_none {A B C :Type} (f:A->B->C) o : option_map2 f o None = None.
+Definition pure {A} : A -> option A := fun x => Some x.
+
+Infix ">>=" := bind (at level 55, right associativity).
+
+Definition liftM {A B} (f : A -> B) (x : option A) : option B :=
+ x >>= (pure ∘ f).
+
+Definition liftM2 {A B C} (f : A -> B -> C) (x : option A) (y : option B) : option C :=
+ x >>= (fun x' => y >>= pure ∘ f x').
+
+Fixpoint mapM {A B} (f : A -> option B) (l : list A) : option (list B) :=
+  match l with
+    | nil => Some nil
+    | x :: xs => liftM2 (fun x' xs' => x' :: xs') (f x) (mapM f xs)
+  end.
+
+
+Lemma liftM2_none {A B C :Type} (f:A->B->C) o : liftM2 f o None = None.
 Proof.
   destruct o; reflexivity.
 Qed.
@@ -145,49 +146,189 @@ Fixpoint Acc_sem {A} (f : nat -> A -> A) (n : nat) (z : A) : A :=
 
 Reserved Notation "'E'[|' e '|]'" (at level 9).
 
+Import ListNotations.
 
-
-Definition BinOpSem {s t} (op : BinOp s t) : TySem s -> TySem s -> TySem t :=
-  match op in BinOp s t return TySem s -> TySem s -> TySem t with
-    | Add => Rplus
-    | Sub => Rminus
-    | Mult => Rmult
-    | And => andb
-    | Or => orb
-    | Less => Rltb
-    | Equal => Rleb
+Fixpoint OpSem (op : Op) (vs : list Val) : option Val :=
+  match op with
+    | Add => match vs with ([RVal x; RVal y ]) => Some (RVal (x + y)) | _ => None end
+    | Sub => match vs with ([RVal x; RVal y ]) => Some (RVal (x - y)) | _ => None end
+    | Mult => match vs with ([RVal x; RVal y ]) => Some (RVal (x * y)) | _ => None end
+    | And => match vs with ([BVal x; BVal y ]) => Some (BVal (x && y)) | _ => None end
+    | Or => match vs with ([BVal x; BVal y ]) => Some (BVal (x || y)) | _ => None end
+    | Less => match vs with ([RVal x; RVal y ]) => Some (BVal (Rleb x y)) | _ => None end
+    | Equal => match vs with ([RVal x; RVal y ]) => Some (BVal (Reqb x y)) | _ => None end
+    | BLit b => match vs with ([]) => Some (BVal b) | _ => None end
+    | RLit r => match vs with ([]) => Some (RVal r) | _ => None end
+    | Cond => match vs with
+                | ([BVal b; RVal x; RVal y ]) => Some (RVal (if b then x else y))
+                | ([BVal b; BVal x; BVal y ]) => Some (BVal (if b then x else y))
+                | _ => None end
+    | Neg => match vs with ([RVal x]) => Some (RVal (0 - x) %R) | _ => None end
+    | Not => match vs with ([BVal x]) => Some (BVal (negb x)) | _ => None end
   end.
 
-Definition UnOpSem {s t} (op : UnOp s t) : TySem s -> TySem t :=
-  match op in UnOp s t return TySem s -> TySem t with
-    | Not => negb
-    | Neg => fun x => (0 - x) %R
+
+Definition Env := list (option Val).
+
+Fixpoint lookupEnv (v : Var) (rho : Env) : option Val :=
+  match v, rho with
+    | V1, x::_ => x
+    | VS v, _::xs => lookupEnv v xs
+    | _,_ => None
   end.
 
-Fixpoint Esem' {t l} (e : exp' l t) : Env (option ∘ TySem) l -> ext -> option [|t|] :=
+Fixpoint Esem' (e : Exp) (rho : Env) (erho : ExtEnv) : option Val :=
     match e with
-      | Lit r => fun vars rho => Some r
-      | BinOpE _ op e1 e2 => fun vars rho =>  option_map2 (BinOpSem op) (E'[|e1|] vars rho) (E'[|e2|] vars rho)
-      | UnOpE _ op e => fun vars rho => option_map (UnOpSem op) (E'[|e|] vars rho)
-      | IfE b e1 e2 => fun vars rho => match E'[|b|] vars rho with
-                                             | Some true => E'[|e1|] vars rho
-                                             | Some false => E'[|e2|] vars rho
-                                             | None => None
-                                           end
-      | Obs obs t => fun vars rho => rho t _ obs
-      | VarE v => fun vars rho => lookupEnv v vars
-      | Acc f l z => fun vars rho => 
-                          let rho' := adv_ext (- Z.of_nat l) rho
-                          in Acc_sem (fun m x => E'[| f |] (EnvCons x vars) 
-                                            (adv_ext (Z.of_nat m) rho')) l (E'[|z|] vars rho')
+      | OpE op args => (* (mapM (fun e => E'[|e|] rho erho) args) >>= OpSem op *)
+        let fix run l :=  
+            match l with
+              | nil => Some nil
+              | x :: xs => liftM2 (fun x' xs' => x' :: xs') (E'[| x |] rho erho) (run xs)
+            end
+        in run args >>= OpSem op
+      | Obs l i => erho l i
+      | VarE v => lookupEnv v rho
+      | Acc f l z => let erho' := adv_ext (- Z.of_nat l) erho
+                     in Acc_sem (fun m x => E'[| f |] (x :: rho) 
+                                              (adv_ext (Z.of_nat m) erho')) l (E'[|z|] rho erho')
     end
       where "'E'[|' e '|]'" := (Esem' e ). 
 
-Definition empty_env := (EnvNil (option ∘ TySem)).
 
-Notation "'E[|' e '|]' r" := (E'[|e|] empty_env r) (at level 9).
+Notation "'E[|' e '|]' r" := (E'[|e|] nil r) (at level 9).
+
+Hint Constructors TypeVal'.
 
 
+Definition TypeEnv (g : TyEnv) (rho : Env) : Prop := forall_zip TypeVal' rho g.
+
+Definition TypeArgs (ts : list Ty) (args : list Val) : Prop := forall_zip TypeVal args ts.
+
+
+Lemma OpSem_typed' {A} op ts t (args : list A) (f : A -> option Val) :
+|-Op op ∶ ts => t -> forall_zip (fun x t => |-V' (f x) ∶ t) args ts -> |-V' (mapM f args >>= OpSem op) ∶ t.
+Proof.
+  intros O T. induction O; 
+  repeat (idtac; match goal with
+      | [H : forall_zip _ _ _ |- _] => inversion H; clear H
+      | [ |- context [match ?x with 
+                      | _ => _ 
+                    end]] => is_var x; destruct x
+      | [ |- context [match ?x ?y with 
+                      | _ => _ 
+                    end]] => is_var x; is_var y; destruct (x y)
+       
+  end; subst; simpl; unfold liftM2, bind, pure,compose; auto).
+Qed.
+
+Lemma OpSem_typed_total {A} op ts t (args : list A) (f : A -> option Val) :
+|-Op op ∶ ts => t -> forall_zip (fun x t => |-V' (f x) ∶ t /\ exists v, f x = Some v) args ts -> 
+                     exists v, (mapM f args >>= OpSem op) = Some v.
+Proof.
+  intros O T. induction O;
+  repeat (match goal with
+      | [ H : _ /\ _ |- _ ] => 
+        let H1 := fresh in let H2 := fresh in let H3 := fresh in
+        let H4 := fresh in let H5 := fresh in let H6 := fresh in
+        let H7 := fresh in
+        destruct H as [H1 H2]; destruct H2 as [H3 H4]; rewrite H4 in *
+      ; inversion H1 as [ H5 H6 H7| H5 ]; inversion H7
+      | [H : forall_zip _ _ _ |- _] => inversion H; clear H
+  end; subst; simpl; unfold liftM2, bind); eexists; auto.
+Qed.
+
+Lemma EsemOpE op es rho erho : 
+  E'[|OpE op es|] rho erho = (mapM (fun e => E'[|e|] rho erho) es) >>= OpSem op.
+Proof.
+  simpl. f_equal.
+  induction es.
+  + reflexivity.
+  + simpl. f_equal. apply IHes.
+Qed.
+
+
+Lemma lookupEnv_typed g rho v t : TypeEnv g rho -> g |-X v ∶ t -> |-V' (lookupEnv v rho) ∶ t.
+Proof.
+  intros E V. generalize dependent rho. dependent induction V; intros.
+  + simpl. inversion E. auto.
+  + simpl. inversion E. apply IHV. auto.
+Qed.
+
+Lemma adv_ext_step n erho : ((adv_ext (- Z.of_nat (S n)) erho) = (adv_ext (- Z.of_nat n) (adv_ext (-1) erho))).
+Proof.
+  rewrite adv_ext_iter. f_equal. rewrite Nat2Z.inj_succ. omega.
+Qed.
+
+Theorem Esem_typed' g e t (rho : Env) (erho : ExtEnv) : 
+  g |-E e ∶ t -> TypeEnv g rho -> TypeExt erho -> |-V' E'[|e|] rho erho ∶ t.
+Proof.
+  intros E R V'. generalize dependent rho. generalize dependent erho.
+  dependent induction E using TypeExp_ind'; intros.
+  + rewrite EsemOpE. eapply OpSem_typed'. apply H. 
+    do 4 (eapply forall_zip_apply_dep in H1;eauto).  
+  + apply V'. auto.
+  + simpl. eapply lookupEnv_typed; eauto.
+  + simpl. generalize dependent erho. induction n; intros.
+    - simpl. auto.
+    - apply IHE1. auto. constructor. 
+      rewrite adv_ext_step. apply IHn. auto. auto.
+Qed.
+
+Corollary Esem_typed e t (erho : ExtEnv) : 
+  nil |-E e ∶ t -> TypeExt erho -> |-V' E[|e|] erho ∶ t.
+Proof.
+  intros. eapply Esem_typed'; eauto. constructor.
+Qed.
+
+Definition total_ext (erho : ExtEnv) : Prop := forall l i, exists v, erho l i = Some v.
+
+Lemma total_ext_adv d e : total_ext e -> total_ext (adv_ext d e).
+Proof.
+  unfold total_ext. intros. apply H.
+Qed.
+
+Hint Resolve total_ext_adv.
+
+Inductive forall_list {A} (P : A -> Prop) : list A -> Prop :=
+| forall_nil : forall_list P []
+| forall_cons x xs : P x -> forall_list P xs -> forall_list P (x :: xs).
+
+Definition total_env (rho : Env) : Prop := forall_list (fun x => exists v , x = Some v) rho.
+
+
+Theorem Esem_typed_total' g e t (rho : Env) (erho : ExtEnv) : 
+  total_env rho -> total_ext erho -> 
+  g |-E e ∶ t -> TypeEnv g rho -> TypeExt erho -> exists v, E'[|e|] rho erho = Some v.
+Proof.
+  intros T' T E R V'. generalize dependent rho. generalize dependent erho.
+  dependent induction E using TypeExp_ind'; intros.
+  + rewrite EsemOpE in *. eapply OpSem_typed_total. apply H. 
+    apply forall_zip_and. eapply forall_zip_impl in H0; auto.
+    apply H0. intros. eapply Esem_typed'; eauto. 
+    do 6 (eapply forall_zip_apply_dep in H1;eauto). 
+  + apply T.
+  + simpl. generalize dependent rho. 
+    generalize dependent g. induction v; intros.
+    - inversion H. subst. inversion R. subst. simpl.
+      inversion T'. subst. auto.
+    - simpl. inversion H. subst. inversion R. subst. eapply IHv. 
+      apply H2. inversion T'. subst. auto. auto.
+  + apply proj1 with (B:= |-V' E'[|Acc e1 n e2|] rho erho ∶ t).
+    simpl. generalize dependent erho. induction n; intros.
+    - simpl. split. apply IHE2; auto. eapply Esem_typed'; eauto.
+    - rewrite adv_ext_step. split. apply IHE1; auto. constructor;auto. apply IHn; auto.
+      constructor. eapply proj2. apply IHn; auto.
+      auto. eapply Esem_typed'; eauto.
+      constructor. eapply proj2. apply IHn; eauto. auto.
+Qed.
+
+Corollary Esem_typed_total e t (erho : ExtEnv) : 
+  total_ext erho -> nil |-E e ∶ t -> TypeExt erho -> exists v, E[|e|] erho = Some v /\ |-V v ∶ t.
+Proof.
+  intros. pose H0 as H'. eapply Esem_typed_total' in H'; eauto. destruct H'.
+  eexists. split; eauto. eapply Esem_typed in H0; eauto. rewrite H2 in *. inversion H0. 
+  auto. constructor. constructor.
+Qed.
 
 (* Semantic structures for contracts. *)
 
@@ -215,9 +356,9 @@ Definition singleton_trans' (p1 p2 : party) (c : currency) r : trans
                                  else 0.
 Definition singleton_trans (p1 p2 : party) (c : currency) r : transfers  := Some (singleton_trans' p1 p2 c r).
 Definition add_trans' : trans -> trans -> trans := fun t1 t2 p1 p2 c => (t1 p1 p2 c + t2 p1 p2 c).
-Definition add_trans : transfers -> transfers -> transfers := option_map2 add_trans'.
+Definition add_trans : transfers -> transfers -> transfers := liftM2 add_trans'.
 Definition scale_trans' : R -> trans -> trans := fun s t p1 p2 c => (t p1 p2 c * s).
-Definition scale_trans : option R -> transfers -> transfers := option_map2 scale_trans'.
+Definition scale_trans : option R -> transfers -> transfers := liftM2 scale_trans'.
 
 
 Lemma scale_empty_trans' r : scale_trans' r empty_trans' = empty_trans'.
@@ -227,7 +368,7 @@ Qed.
 
 Lemma scale_empty_trans r : scale_trans (Some r) empty_trans = empty_trans.
 Proof.
-  simpl. rewrite scale_empty_trans'. reflexivity.
+  simpl. unfold pure, compose. rewrite scale_empty_trans'. reflexivity.
 Qed.
 
 Lemma add_empty_trans' : add_trans' empty_trans' empty_trans' = empty_trans'.
@@ -237,7 +378,7 @@ Qed.
 
 Lemma add_empty_trans : add_trans empty_trans empty_trans = empty_trans.
 Proof.
-  simpl. rewrite add_empty_trans'. reflexivity.
+  simpl. unfold pure, compose. rewrite add_empty_trans'. reflexivity.
 Qed.
 
 Hint Resolve scale_empty_trans' scale_empty_trans add_empty_trans' add_empty_trans.
@@ -308,15 +449,15 @@ Qed.
 
 (* The following function is needed to define the semantics of [IfWithin]. *)
 
-Fixpoint within_sem (c1 c2 : ext -> trace) 
-         (e : exp BTy) (rc : ext) (i : nat) : trace 
+Fixpoint within_sem (c1 c2 : ExtEnv -> trace) 
+         (e : Exp) (rc : ExtEnv) (i : nat) : trace 
   := match E[|e|]rc with
-       | Some true => c1 rc
-       | Some false => match i with
+       | Some (BVal true) => c1 rc
+       | Some (BVal false) => match i with
                          | O => c2 rc
                          | S j => delay_trace 1 (within_sem c1 c2 e (adv_ext 1 rc) j)
                        end
-       | None => const_trace bot_trans
+       | _ => const_trace bot_trans
      end.
 
 
@@ -324,14 +465,50 @@ Fixpoint within_sem (c1 c2 : ext -> trace)
 
 Reserved Notation "'C[|' e '|]'" (at level 9).
 
-Fixpoint Csem (c : contract) : ext -> trace :=
+Definition toReal (v : Val) : option R := 
+  match v with
+    | RVal r => Some r
+    | BVal _ => None
+  end.
+
+Fixpoint Csem (c : Contr) : ExtEnv -> trace :=
   fun rho => 
     match c with
       | Zero => empty_trace
-      | TransfOne p1 p2 c => singleton_trace (singleton_trans p1 p2 c  1)
-      | Scale e c' => scale_trace E[|e|]rho (C[|c'|]rho) 
-      | Transl d c' => (delay_trace d) (C[|c'|](adv_ext (Z.of_nat d) rho))
+      | Transfer p1 p2 c => singleton_trace (singleton_trans p1 p2 c  1)
+      | Scale e c' => scale_trace (E[|e|]rho >>= toReal) (C[|c'|]rho) 
+      | Translate d c' => (delay_trace d) (C[|c'|](adv_ext (Z.of_nat d) rho))
       | Both c1 c2 => add_trace (C[|c1|]rho) (C[|c2|]rho)
-      | IfWithin e d c1 c2 => within_sem C[|c1|] C[|c2|] e rho d
+      | If e d c1 c2 => within_sem C[|c1|] C[|c2|] e rho d
     end
       where "'C[|' e '|]'" := (Csem e).
+
+Definition total_trace (t : trace) := forall i, exists v, t i = Some v.
+
+Hint Unfold empty_trace empty_trans const_trace empty_trans 
+     total_trace singleton_trace singleton_trans scale_trace scale_trans.
+
+Theorem Csem_typed_total c (erho : ExtEnv) : 
+  total_ext erho -> |-C c -> TypeExt erho -> total_trace (C[|c|] erho).
+Proof.
+  intros T C R. generalize dependent erho. unfold total_trace. induction C.
+  + simpl. repeat autounfold. eauto.
+  + simpl. repeat autounfold. intros. destruct i; eauto.
+  + simpl. repeat autounfold. intros. unfold liftM2, bind.
+    destruct (IHC erho T R i). 
+    eapply Esem_typed_total in H; eauto. decompose [and ex] H.
+    rewrite H2. inversion H3. simpl. rewrite  H0.
+    unfold pure, compose. eauto.
+  + intros. simpl. unfold delay_trace. destruct (leb d i).
+    simpl. apply IHC; auto. autounfold; eauto.
+  + intros. simpl. unfold add_trace, add_trans, liftM2, bind. 
+    destruct (IHC1 erho T R i). rewrite H.
+    destruct (IHC2 erho T R i). rewrite H0.
+    unfold pure, compose. eauto.
+  + simpl. induction d; intros.
+    - simpl. eapply  Esem_typed_total in H; eauto. decompose [and ex] H.
+      rewrite H1. inversion H2. destruct b; auto.
+    - simpl. eapply  Esem_typed_total in H; eauto. decompose [and ex] H.
+      rewrite H1. inversion H2. destruct b; auto. 
+      unfold delay_trace. destruct (leb 1 i). apply IHd; eauto. autounfold. eauto.
+Qed.
--- Coq/Syntax.v
+++ Coq/Syntax.v
@@ -6,93 +6,178 @@ Require Export List.
 Definition currency := string.
 Definition party := string.
 
-Inductive Ty := RTy | BTy.
+Inductive Ty := REAL | BOOL.
 
-Inductive Var : list Ty -> Ty -> Set := 
-| V1 {l} i : Var (i :: l) i
-| VS {l i j} : Var l i -> Var (j :: l) i
-.
+Inductive Var : Set := V1 | VS (v:Var).
 
+Inductive ObsLabel : Set := LabR (l:string) | LabB (l:string).
 
-Inductive Env (ty : Ty -> Type) : list Ty -> Type := 
-  | EnvNil : Env ty nil
-  | EnvCons {i l} : ty i -> Env ty l -> Env ty (i :: l).
+Inductive Val : Set := BVal : bool -> Val | RVal : R -> Val.
 
-Implicit Arguments EnvCons [[ty][i][l]].
-
-Require Import JMeq.
-
-Program Fixpoint lookupEnv  {ty l} {t : Ty} (v : Var l t) : Env ty l -> ty t :=
-  match v in Var l t return Env ty l -> ty t with
-    | V1 _ _ => fun e => match e with
-                       | EnvCons _ _ r _ => r
-                       | EnvNil => _
-                     end
-    | VS l' i j v' => fun e => match e with
-                                 | EnvCons _ l' _ e' =>  @lookupEnv ty l' i v' e'
-                                 | EnvNil => _
-                        end
-  end.
+Inductive Op : Set := Add | Sub | Mult | And | Or | Less | Equal |
+                      Not | Neg |
+                      BLit (b : bool) | RLit (r:R) |
+                      Cond.
 
 
 
-
-Definition TySem (t : Ty) : Set :=
-  match t with
-    | RTy => R
-    | BTy => bool
-  end.
-
-Notation "'[|' t '|]'" := (TySem t) (at level 9).
-
-Inductive ObsLabel : Ty -> Set :=
-  | obsLabel ty : string -> ObsLabel ty.
-
-Inductive BinOp : Ty -> Ty -> Type :=
-| Add : BinOp RTy RTy
-| Sub : BinOp RTy RTy
-| Mult : BinOp RTy RTy
-| And : BinOp BTy BTy
-| Or : BinOp BTy BTy
-| Less : BinOp RTy BTy
-| Equal : BinOp RTy BTy.
-
-Inductive UnOp : Ty -> Ty -> Type :=
-| Not : UnOp BTy BTy
-| Neg : UnOp RTy RTy.
-
-Inductive exp' V t : Set :=
-| Lit : [|t|] -> exp' V t
-| BinOpE {s} : BinOp s t -> exp' V s -> exp' V s -> exp' V t
-| UnOpE {s} : UnOp s t -> exp' V s -> exp' V t
-| IfE : exp' V BTy -> exp' V t -> exp' V t -> exp' V t
-| Obs :  ObsLabel t -> Z -> exp' V t
-| VarE : Var V t -> exp' V t
-| Acc : exp' (t :: V) t -> nat -> exp' V t -> exp' V t. 
+Inductive Exp : Set := OpE (op : Op) (args : list Exp)
+                     | Obs (l:ObsLabel) (i: Z)
+                     | VarE (v:Var)
+                     | Acc (f : Exp) (d : nat) (e : Exp).
 
 
-Implicit Arguments Lit [[V][t]].
-Implicit Arguments BinOpE [[V][t][s]].
-Implicit Arguments UnOpE [[V][t][s]].
-Implicit Arguments IfE [[V][t]].
-Implicit Arguments Obs [[V][t]].
-Implicit Arguments VarE [[V][t]].
-Implicit Arguments Acc [[V][t]].
+Inductive Contr : Set :=
+ | Zero : Contr
+ | Transfer : party -> party -> currency -> Contr
+ | Scale : Exp -> Contr -> Contr
+ | Translate : nat -> Contr -> Contr
+ | Both : Contr -> Contr -> Contr
+ | If : Exp -> nat -> Contr -> Contr -> Contr.
 
 
-Definition exp t := exp' nil t .
-
-Inductive contract : Set :=
- | Zero : contract
- | TransfOne : party -> party -> currency -> contract
- | Scale : exp RTy -> contract -> contract
- | Transl : nat -> contract -> contract
- | Both : contract -> contract -> contract
- | IfWithin : exp BTy -> nat -> contract -> contract -> contract.
-
-
-Definition transl (l : nat) : contract -> contract := 
+Definition translate (l : nat) : Contr -> Contr := 
   match l with
     | O => (fun x => x)
-    | _ => Transl l
+    | _ => Translate l
   end.
+
+Reserved Notation "'|-V' e '∶' t" (at level 20).
+
+Inductive TypeVal : Val -> Ty -> Prop := 
+| type_bool b : |-V BVal b ∶ BOOL
+| type_real b : |-V RVal b ∶ REAL
+        where "'|-V' v '∶' t" := (TypeVal v t).
+
+Reserved Notation "'|-Op' e '∶' t '=>' r" (at level 20).
+
+Import ListNotations.
+
+Inductive TypeOp : Op -> list Ty -> Ty -> Prop := 
+| type_blit b : |-Op (BLit b) ∶ [] => BOOL
+| type_rlit r : |-Op (RLit r) ∶ [] => REAL
+| type_neg : |-Op Neg ∶ [REAL] => REAL
+| type_not : |-Op Not ∶ [BOOL] => BOOL
+| type_cond t : |-Op Cond ∶ [BOOL;t;t] => t
+| type_add : |-Op Add ∶ [REAL;REAL] => REAL
+| type_sub : |-Op Sub ∶ [REAL;REAL] => REAL
+| type_mult : |-Op Mult ∶ [REAL;REAL] => REAL
+| type_and : |-Op And ∶ [BOOL;BOOL] => BOOL
+| type_or : |-Op Or ∶ [BOOL;BOOL] => BOOL
+| type_less : |-Op Less ∶ [REAL;REAL] => BOOL
+| type_equal : |-Op Equal ∶ [REAL;REAL] => BOOL
+        where "'|-Op' v '∶' t '=>' r" := (TypeOp v t r).
+
+Reserved Notation "'|-O' e '∶' t" (at level 20).
+
+Inductive TypeObs : ObsLabel -> Ty -> Prop := 
+| type_obs_bool b : |-O LabB b ∶ BOOL
+| type_obs_real b : |-O LabR b ∶ REAL
+        where "'|-O' v '∶' t" := (TypeObs v t).
+
+Reserved Notation "g '|-X' v '∶' t" (at level 20).
+
+Definition TyEnv := list Ty.
+
+Inductive TypeVar : TyEnv -> Var -> Ty -> Prop :=
+| type_var_1 t g  : (t :: g) |-X V1 ∶ t
+| type_var_S g v t : g |-X v ∶ t -> (t :: g) |-X VS v ∶ t
+        where "g '|-X' v '∶' t" := (TypeVar g v t).
+
+
+Inductive forall_zip {A B} (R : A -> B -> Prop) : list A -> list B -> Prop :=
+| forall_zip_nil : forall_zip R [] []
+| forall_zip_cons x y xs ys : R x y -> forall_zip R xs ys -> forall_zip R (x::xs) (y::ys).
+
+Hint Constructors forall_zip.
+
+
+Lemma forall_zip_impl {A B} (R1 R2 : A -> B -> Prop) xs ys : 
+  (forall x y, R1 x y -> R2 x y) -> forall_zip R1 xs ys -> forall_zip R2 xs ys.
+Proof.
+  intros f l. induction l; auto. 
+Qed.
+
+
+Lemma forall_zip_apply {A B} (R : A -> B -> Prop) (P : Prop) xs ys : 
+  forall_zip (fun x y => P -> R x y) xs ys -> P -> forall_zip R xs ys.
+Proof.
+  intros F p. induction F; auto.
+Qed.
+
+Lemma forall_zip_apply_dep {A B} (P : Type) (p : P) (R : A -> B -> P -> Prop) xs ys : 
+  forall_zip (fun x y => forall (p:P), R x y p) xs ys -> forall_zip (fun x y => R x y p) xs ys.
+Proof.
+  intros F. induction F; auto.
+Qed.
+
+
+Lemma forall_zip_and {A B} (R1 R2 : A -> B -> Prop) xs ys : 
+  forall_zip R1 xs ys -> forall_zip R2 xs ys -> forall_zip (fun x y => R1 x y /\ R2 x y) xs ys.
+Proof.
+  intros A1 A2. induction A1; inversion A2; auto.
+Qed.
+
+
+Reserved Notation "g '|-E' e '∶' t" (at level 20).
+
+Inductive TypeExp : TyEnv -> Exp -> Ty -> Prop :=
+| type_op g op es ts t : |-Op op ∶ ts => t -> forall_zip (TypeExp g) es ts -> g |-E OpE op es ∶ t
+| type_obs t g o z : |-O o ∶ t -> g |-E Obs o z ∶ t
+| type_var t g v : g |-X v ∶ t -> g |-E VarE v ∶ t
+| type_acc n t g e1 e2 : (t :: g) |-E e1 ∶ t -> g |-E e2 ∶ t -> g |-E Acc e1 n e2 ∶ t
+        where "g '|-E' e '∶' t" := (TypeExp g e t).
+
+(* The induction principle generated by Coq is not strong enough. We
+need to roll our own. *)
+
+Definition TypeExp_ind' : forall P : TyEnv -> Exp -> Ty -> Prop,
+       (forall (g : TyEnv) (op : Op) (es : list Exp) (ts : list Ty) (t : Ty),
+        |-Op op ∶ ts => t ->
+        forall_zip (TypeExp g) es ts -> forall_zip (P g) es ts -> P g (OpE op es) t) ->
+       (forall (t : Ty) (g : TyEnv) (o : ObsLabel) (z : Z),
+        |-O o ∶ t -> P g (Obs o z) t) ->
+       (forall (t : Ty) (g : TyEnv) (v : Var), g |-X v ∶ t -> P g (VarE v) t) ->
+       (forall (n : nat) (t : Ty) (g : list Ty) (e1 e2 : Exp),
+        (t :: g) |-E e1 ∶ t ->
+        P (t :: g) e1 t -> g |-E e2 ∶ t -> P g e2 t -> P g (Acc e1 n e2) t) ->
+       forall (t : TyEnv) (e : Exp) (t0 : Ty), t |-E e ∶ t0 -> P t e t0 :=
+  fun (P : TyEnv -> Exp -> Ty -> Prop)
+  (f : forall (g : TyEnv) (op : Op) (es : list Exp) (ts : list Ty) (t : Ty),
+       |-Op op ∶ ts => t -> forall_zip (TypeExp g) es ts -> forall_zip (P g) es ts -> P g (OpE op es) t)
+  (f0 : forall (t : Ty) (g : TyEnv) (o : ObsLabel) (z : Z),
+        |-O o ∶ t -> P g (Obs o z) t)
+  (f1 : forall (t : Ty) (g : TyEnv) (v : Var), g |-X v ∶ t -> P g (VarE v) t)
+  (f2 : forall (n : nat) (t : Ty) (g : list Ty) (e1 e2 : Exp),
+        (t :: g) |-E e1 ∶ t ->
+        P (t :: g) e1 t -> g |-E e2 ∶ t -> P g e2 t -> P g (Acc e1 n e2) t) =>
+fix F (t : TyEnv) (e : Exp) (t0 : Ty) (t1 : t |-E e ∶ t0) {struct t1} :
+  P t e t0 :=
+  match t1 in (t2 |-E e0 ∶ t3) return (P t2 e0 t3) with
+  | type_op g op es ts t2 t3 f3 =>
+    let fix step es ts (args: forall_zip (TypeExp g) es ts) :=
+        match args with
+          | forall_zip_nil => forall_zip_nil (P g)
+          | forall_zip_cons e t0 es ts ty tys => forall_zip_cons (P g) e t0 es ts (F g e t0 ty) (step es ts tys)
+        end
+          in f g op es ts t2 t3 f3 (step es ts f3)
+  | type_obs t2 g o z t3 => f0 t2 g o z t3
+  | type_var t2 g v t3 => f1 t2 g v t3
+  | type_acc n t2 g e1 e2 t3 t4 =>
+      f2 n t2 g e1 e2 t3 (F (t2 :: g) e1 t2 t3) t4 (F g e2 t2 t4)
+  end.
+
+
+Reserved Notation "'|-C' e" (at level 20).
+
+Inductive TypeContr : Contr -> Prop :=
+| type_zero : |-C Zero
+| type_transfer p1 p2 c : |-C Transfer p1 p2 c
+| type_scale e c : nil |-E e ∶ REAL -> |-C c -> |-C Scale e c
+| type_translate d c : |-C c -> |-C Translate d c
+| type_both c1 c2 : |-C c1 ->  |-C c2 -> |-C Both c1 c2
+| type_if e d c1 c2 : nil |-E e ∶ BOOL -> |-C c1 ->  |-C c2 -> |-C If e d c1 c2
+  where "'|-C' c" := (TypeContr c).
+
+
+Hint Constructors TypeVal TypeOp TypeObs TypeExp TypeVar TypeContr.
