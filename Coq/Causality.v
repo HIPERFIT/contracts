a1fb04d59aeac4222ac54d496ceec954be38b3c2
Patrick Bahr <patrick.bahr@gmail.com>
2014-11-06 16:44:56 +0100

    syntactic causality

--- Coq/Causality.v
+++ Coq/Causality.v
@@ -7,20 +7,20 @@ Open Scope Z.
 
 (* [obs_until d r1 r2] iff [r1] [r2] coincide at [d] and earlier. *)
 
-Definition ext_until (d : Z) (r1 r2 : ext) : Prop :=
-  forall z, Z.le z d -> r1 z = r2 z.
+Definition ext_until (d : Z) (r1 r2 : ExtEnv) : Prop :=
+  forall l z, Z.le z d -> r1 l z = r2 l z.
 
 (* Semantic causality *)
 
-Definition causal (c : contract) : Prop :=
+Definition causal (c : Contr) : Prop :=
   forall d r1 r2,  ext_until (Z.of_nat d) r1 r2 -> (C[|c|]r1) d = (C[|c|]r2) d.
 
 
-Lemma ext_until_adv d t (r1 r2 : ext): 
+Lemma ext_until_adv d t (r1 r2 : ExtEnv): 
   ext_until d (adv_ext t r1) (adv_ext t r2) <-> ext_until (t + d) r1 r2.
 Proof.
   unfold ext_until,adv_ext. split; intros.
-  - pose (H (z - t)%Z). 
+  - pose (H l (z - t)%Z). 
     assert (t + (z - t) = z)%Z as E. omega. rewrite E in *. 
     apply e. omega.
   - apply H. omega.
@@ -35,7 +35,7 @@ Proof.
   rewrite ext_until_adv. rewrite H1. assumption.
 Qed.
 
-Lemma ext_until_le d1 d2 (r1 r2 : ext) : ext_until d1 r1 r2 ->  d2 <= d1 -> ext_until d2 r1 r2.
+Lemma ext_until_le d1 d2 (r1 r2 : ExtEnv) : ext_until d1 r1 r2 ->  d2 <= d1 -> ext_until d2 r1 r2.
 Proof. 
   unfold ext_until. intros. apply H. omega.
 Qed.
--- Coq/SyntacticCausality.v
+++ Coq/SyntacticCausality.v
@@ -7,72 +7,90 @@ short a contract is syntactically causal if observables and external
 choices are never queried at a positive offset. *)
 
 
-Reserved Notation "'E|-' c" (at level 20).
+Inductive Epc : Exp -> Prop:=
+| epc_obs : forall o i, i <= 0 -> Epc (Obs o i)
+| epc_op : forall op es, forall_list Epc es -> Epc (OpE op es)
+| epc_var : forall v, Epc (VarE v)
+| epc_acc : forall f l z, Epc f -> Epc z -> Epc (Acc f l z).
 
-Open Scope Z.
 
-Inductive epc : forall {V t}, exp' V t -> Prop:=
-| epc_obs : forall t V o i, i <= 0 -> E|- Obs o i (V:=V) (t:=t)
-| epc_lit : forall t V q, E|- Lit q (V:=V) (t:=t)
-| epc_bin : forall t s V op e1 e2, E|- e1 -> E|- e2 -> E|- @BinOpE t s V op e1 e2
-| epc_if : forall t V b e1 e2, E|- b -> E|- e1 -> E|- e2 -> E|- @IfE t V b e1 e2
-| epc_un : forall t s V op e, E|- e -> E|- @UnOpE t s V op e
-| epc_var : forall t V q, E|- @VarE t V q 
-| epc_acc : forall t V f l z, E|- f -> E|- z -> E|- @Acc t V f l z
-                                         where "'E|-' e" := (epc e). 
+(* Custom induction principle *)
 
-Reserved Notation "'|-' c" (at level 20).
+Definition Epc_ind' : forall P : Exp -> Prop,
+       (forall (o : ObsLabel) (i : Z), i <= 0 -> P (Obs o i)) ->
+       (forall (op : Op) (es : list Exp), forall_list Epc es -> forall_list P es -> P (OpE op es)) ->
+       (forall v : Var, P (VarE v)) ->
+       (forall (f2 : Exp) (l : nat) (z : Exp),
+        Epc f2 -> P f2 -> Epc z -> P z -> P (Acc f2 l z)) ->
+       forall e : Exp, Epc e -> P e
+  := 
+fun (P : Exp -> Prop)
+  (f : forall (o : ObsLabel) (i : Z), i <= 0 -> P (Obs o i))
+  (f0 : forall (op : Op) (es : list Exp), forall_list Epc es -> forall_list P es -> P (OpE op es))
+  (f1 : forall v : Var, P (VarE v))
+  (f2 : forall (f2 : Exp) (l : nat) (z : Exp),
+        Epc f2 -> P f2 -> Epc z -> P z -> P (Acc f2 l z)) =>
+fix F (e : Exp) (e0 : Epc e) {struct e0} : P e :=
+  match e0 in (Epc e1) return (P e1) with
+  | epc_obs o i l => f o i l
+  | epc_op op es f3 => let fix step {es} (ps : forall_list Epc es) : forall_list P es := 
+                           match ps in forall_list _ es return forall_list P es with
+                             | forall_nil => forall_nil P
+                             | forall_cons e es p ps' => forall_cons P (F e p) (step ps')
+                           end
+                       in f0 op es f3 (step f3)
+  | epc_var v => f1 v
+  | epc_acc f3 l z e1 e2 => f2 f3 l z e1 (F f3 e1) e2 (F z e2)
+  end.
 
-Inductive pc : contract -> Prop :=
-| pc_transl : forall d c, |- c -> |- Transl d c
-| pc_transf : forall cur p1 p2, |- TransfOne cur p1 p2
-| pc_scale : forall e c, E|- e -> |- c -> |- Scale e c
-| pc_both : forall c1 c2, |- c1 -> |- c2 -> |- Both c1 c2
-| pc_zero : |- Zero
-| pc_if : forall c1 c2 b l, E|- b -> |- c1 -> |- c2 -> |- IfWithin b l c1 c2
-                                            where "'|-' c" := (pc c). 
 
-Hint Constructors epc pc.
+
+Inductive Pc : Contr -> Prop :=
+| pc_transl : forall d c, Pc c -> Pc (Translate d c)
+| pc_transf : forall cur p1 p2, Pc (Transfer cur p1 p2)
+| pc_scale : forall e c, Epc e -> Pc c -> Pc (Scale e c)
+| pc_both : forall c1 c2, Pc c1 -> Pc c2 -> Pc (Both c1 c2)
+| pc_zero : Pc Zero
+| pc_if : forall c1 c2 b l, Epc b -> Pc c1 -> Pc c2 -> Pc (If b l c1 c2).
+
+
+Hint Constructors Epc Pc.
 
 (* Below follows the proof that provable causality is sound (i.e. it
 implies semantic causality). *)
 
-Lemma epc_ext_until' V t (e : exp' V t) d r1 r2 vars : 
-  E|-e -> 0 <= d -> ext_until d r1 r2 -> E'[|e|]vars r1 = E'[|e|]vars r2.
+Lemma epc_ext_until' (e : Exp) d r1 r2 vars : 
+  Epc e -> 0 <= d -> ext_until d r1 r2 -> E'[|e|]vars r1 = E'[|e|]vars r2.
 Proof.
   intros R D O.   generalize dependent vars. generalize dependent r2. generalize dependent r1.
-  induction R; intros; simpl; try solve [f_equal; auto].
-  - unfold ext_until in O. rewrite O. reflexivity. omega.
-  - erewrite IHR1 by eassumption. erewrite IHR2 by eassumption. erewrite IHR3 by eassumption.
-    reflexivity.
-  - remember (adv_ext (- Z.of_nat l) r1) as r1'.
-    remember (adv_ext (- Z.of_nat l) r2) as r2'.
-    assert (ext_until (Z.of_nat l + d) r1' r2') as I.
-    subst. rewrite ext_until_adv. 
-    assert (- Z.of_nat l + (Z.of_nat l + d) = d) as L.
-    omega. rewrite L. assumption.
-    clear Heqr1' Heqr2'.
-    induction l. 
-    + simpl. apply IHR2. simpl in I. assumption.
-    + simpl. rewrite IHl. apply IHR1. rewrite ext_until_adv.
-      eapply ext_until_le. eassumption. simpl. omega.
-      eapply ext_until_le. apply I. rewrite Nat2Z.inj_succ. omega.
+  induction R using Epc_ind'; intros; try solve [simpl; f_equal; auto].
+  - simpl; unfold ext_until in O. rewrite O. reflexivity. omega.
+  - repeat rewrite EsemOpE. 
+    do 4 (eapply forall_list_apply_dep in H0;eauto).
+    apply mapM_rewrite in H0. rewrite H0. reflexivity.
+  - generalize dependent vars. generalize dependent r2. generalize dependent r1. induction l; intros. 
+    + simpl. apply IHR2. assumption.
+    + pose (adv_ext_step l) as RE. simpl in *. do 2 rewrite RE.
+      erewrite IHl. apply IHR1. rewrite ext_until_adv.
+      do 2 rewrite adv_ext_iter. apply ext_until_adv. 
+      eapply ext_until_le.  apply O. rewrite Zpos_P_of_succ_nat.
+      omega. apply ext_until_adv. eapply ext_until_le.  apply O. omega.
 Qed.
 
-Corollary epc_ext_until t (e : exp t) d r1 r2 : 
-  E|-e -> 0 <= d -> ext_until d r1 r2 -> E[|e|] r1 = E[|e|]r2.
+Corollary epc_ext_until (e : Exp) d r1 r2 : 
+  Epc e -> 0 <= d -> ext_until d r1 r2 -> E[|e|] r1 = E[|e|]r2.
 Proof. apply epc_ext_until'. Qed.
 
 
 
-Theorem pc_causal c : |- c -> causal c.
+Theorem pc_causal c : Pc c -> causal c.
 Proof.
   intros. induction H; unfold causal in *; intros; simpl.
 
   unfold delay_trace.
   remember (leb d d0) as C. destruct C.
     symmetry in HeqC. apply leb_complete in HeqC.
-    apply IHpc. rewrite ext_until_adv. assert (Z.of_nat d + Z.of_nat(d0 - d) = Z.of_nat d0) as D.
+    apply IHPc. rewrite ext_until_adv. assert (Z.of_nat d + Z.of_nat(d0 - d) = Z.of_nat d0) as D.
     rewrite <- Nat2Z.inj_add. f_equal. omega.
     rewrite D. assumption.
     
@@ -80,7 +98,7 @@ Proof.
 
   reflexivity.
 
-  unfold scale_trace, compose. erewrite IHpc by apply H1.
+  unfold scale_trace, compose. erewrite IHPc by apply H1.
   unfold scale_trans. rewrite epc_ext_until with (r2:=r2) (d:=Z.of_nat d) by (auto; omega).
   reflexivity. 
 
@@ -92,52 +110,67 @@ Proof.
   induction l; intros; simpl.
   
   rewrite epc_ext_until with (r2:=r2) (d:=Z.of_nat d) by (eauto;omega). 
-  remember (E[|b|]r2) as bl. destruct bl. destruct t. eapply IHpc1; eassumption. 
-  eapply IHpc2; eassumption. reflexivity. 
+  remember (E[|b|]r2) as bl. destruct bl. destruct v. destruct b0. eapply IHPc1; eassumption. 
+  eapply IHPc2; eassumption. reflexivity. reflexivity.
 
   rewrite epc_ext_until with (r2:=r2) (d:=Z.of_nat d) by (eauto;omega). 
-  remember (E[|b|]r2) as bl. destruct bl. destruct t.  eapply IHpc1; eassumption. 
+  remember (E[|b|]r2) as bl. destruct bl. destruct v. destruct b0.  eapply IHPc1; eassumption. 
   unfold delay_trace. remember (leb 1 d) as L. destruct L.  apply IHl. 
   rewrite Nat2Z.inj_sub.
   apply ext_until_adv_1. symmetry in HeqL. apply leb_complete in HeqL. apply inj_le in HeqL. auto.
   auto.
-  apply leb_complete. auto. auto. reflexivity. 
+  apply leb_complete. auto. auto. reflexivity. reflexivity.
 Qed.
 
 Open Scope bool.
 
-Fixpoint epc_dec {V t} (e : exp' V t) : bool :=
+Fixpoint epc_dec (e : Exp) : bool :=
   match e with
     | Obs _ i => Z.leb i 0
-    | Lit _ => true
-    | BinOpE _ _ e1 e2 => epc_dec e1 && epc_dec e2
-    | UnOpE _ _ e' => epc_dec e'
-    | IfE b e1 e2 => epc_dec b && epc_dec e1 && epc_dec e2
+    | OpE _ args => let fix run es := 
+                           match es with
+                             | nil => true
+                             | e' :: es' => epc_dec e' && run es'
+                           end
+                       in run args
     | VarE _ => true
     | Acc f _ z => epc_dec f && epc_dec z
   end.
 
-Lemma epc_dec_correct {V t} (e : exp' V t) : epc_dec e = true <-> E|- e. 
+Require Import Tactics.
+
+Lemma epc_dec_correct (e : Exp) : epc_dec e = true <-> Epc e. 
 Proof.
   split.
-  - intro D. induction e; simpl in *; 
-    try first [rewrite Z.leb_le in D|
+  - intro D. induction e using Exp_ind'; simpl in *; try first [rewrite Z.leb_le in D|
                repeat rewrite Bool.andb_true_iff in D; decompose [and] D]; auto.
-  - intros D. induction D; simpl; try first [rewrite IHD1, IHD2| apply Z.leb_le]; auto.
+    constructor. induction H. 
+    + auto.
+    + constructor.  destruct (epc_dec x); tryfalse. auto.
+      apply IHforall_list. destruct ((fix run (es : list Exp) : bool :=
+      match es with
+      | Datatypes.nil => true
+      | e' :: es' => epc_dec e' && run es'
+      end) xs). reflexivity. rewrite Bool.andb_false_r in *. tryfalse.
+
+  - intros D. induction D using Epc_ind'; try first [simpl; rewrite IHD1, IHD2| apply Z.leb_le]; auto.
+    induction H0.
+    + auto.
+    + simpl in *. rewrite IHforall_list. rewrite H0. reflexivity. inversion H. auto.
 Qed.
 
 
-Fixpoint pc_dec (c : contract) : bool :=
+Fixpoint pc_dec (c : Contr) : bool :=
   match c with
     | Zero => true
-    | TransfOne _ _ _ => true
+    | Transfer _ _ _ => true
     | Scale e c => epc_dec e && pc_dec c
-    | Transl _ c => pc_dec c
+    | Translate _ c => pc_dec c
     | Both c1 c2 => pc_dec c1 && pc_dec c2
-    | IfWithin e _ c1 c2 => epc_dec e && pc_dec c1 && pc_dec c2
+    | If e _ c1 c2 => epc_dec e && pc_dec c1 && pc_dec c2
   end.
 
-Theorem pc_dec_correct (c : contract) : pc_dec c = true <-> |- c. 
+Theorem pc_dec_correct (c : Contr) : pc_dec c = true <-> Pc c. 
 Proof.
   split.
   - intro D. induction c; simpl in *; 
