d10787a01004b7fc8953028e9c51cfaa098b63be
Patrick Bahr <patrick.bahr@gmail.com>
2015-01-22 10:19:08 +0100

    add American Option to the EDSL examples

--- Coq/Extraction/Examples/AsianOption.hs
+++ Coq/Extraction/Examples/AsianOption.hs
@@ -1,7 +1,16 @@
-import EDSL
+{-# LANGUAGE RebindableSyntax #-}
+
+import RebindableEDSL
 
 option :: Contr
-option = translate 90 (iff (bObs "X" 0) trade zero)
-    where trade  = scale 100 (both (transfer "Y" "X" "USD") pay)
-          pay    = scale rate (transfer "X" "Y" "DKK")
-          rate   = (acc (\r -> r + (rObs "FX USD/DKK" 0))  30 0) / 30
+option = 90 ! if bObs "X" 0 
+              then 100 # (transfer "Y" "X" "USD" & 
+                          (rate # transfer "X" "Y" "DKK"))
+              else zero
+    where rate   = (acc (\r -> r + rObs "FX USD/DKK" 0)  30 0) / 30
+
+american :: Contr
+american = if bObs "X" 0 `within` 90 
+           then 100 # (transfer "Y" "X" "USD" & 
+                       (6.23 # transfer "X" "Y" "DKK"))
+           else zero
