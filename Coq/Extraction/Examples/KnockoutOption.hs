f9e16762d0e624fe9dfa54875e9348fd6cb863d5
Patrick Bahr <patrick.bahr@gmail.com>
2015-02-09 17:18:43 +0100

    knock-out barrier option

---
+++ Coq/Extraction/Examples/KnockoutOption.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RebindableSyntax #-}
+
+import RebindableEDSL
+
+-- knock-out barrier option
+--
+
+knockout :: Exp R -> Exp R -> Exp R -> Int -> Contr
+knockout barrier strike notional maturity = 
+    if rObs (FX EUR DKK) 0 <= barrier `within` maturity
+    then zero
+    else if bObs (Decision X "exercise") 0
+         then notional # (transfer Y X EUR & 
+              (strike # transfer X Y DKK))
+         else zero
