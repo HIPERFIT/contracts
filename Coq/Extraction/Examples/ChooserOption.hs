70092bddb1769a3102fea5a5d0cc9affc4b7dcc5
Patrick Bahr <patrick.bahr@gmail.com>
2015-02-20 16:38:48 +0100

    chooser option example

---
+++ Coq/Extraction/Examples/ChooserOption.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RebindableSyntax #-}
+
+import RebindableEDSL
+
+strike :: Exp R
+strike = 6.5
+
+contract :: Contr
+contract = do price <- rObs (FX DKK USD) 60
+              payout <- ife (bObs (Decision X "call option") 30)
+                         (max (price - strike) 0)
+                         (max (strike - price) 0)
+              60 ! (payout # transfer Y X DKK)
