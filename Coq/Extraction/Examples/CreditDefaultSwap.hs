059617f838027001d8d7a2210b1c278da2f892dd
Patrick Bahr <patrick.bahr@gmail.com>
2015-02-20 16:54:53 +0100

    fix CDS example

--- Coq/Extraction/Examples/CreditDefaultSwap.hs
+++ Coq/Extraction/Examples/CreditDefaultSwap.hs
@@ -12,9 +12,9 @@ cds months cur premium comp buyer seller ref =
     step months
     where step i = if (i <= 0) then zero else
                       (premium # transfer buyer seller cur) &  
-                      if bObs (Default ref) 0 `within` 30 * months
+                      if bObs (Default ref) 0 `within` 30
                       then comp # transfer seller buyer cur
-                      else 30 ! step (i-1)
+                      else step (i-1)
 
 
 -- A bond contract parametrised by the maturity (in months), the
@@ -31,4 +31,13 @@ bond months cur inter nom holder issuer = step months
 
 
 bondCDSExample :: Contr
-bondCDSExample = bond 12 DKK 10 1000 X Y & cds 12 DKK 9 1000 Y Z X 
+bondCDSExample = bond 12 DKK 10 1000 Y X & cds 12 DKK 9 1000 Y Z X 
+
+env1 = mkExtEnvP [] [(Default X,n,False) | n <- [0..6*30]]
+env2 = mkExtEnvP [] [(Default X,n,n==50) | n <- [0..6*30]]
+
+spec1 :: Contr
+spec1 = specialise bondCDSExample env1
+
+spec2 :: Contr
+spec2 = specialise bondCDSExample env2
